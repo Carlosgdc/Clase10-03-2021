6f648b6b467635f8c3f32d681ad3013b0b719de2
Carlosgdc <cdiazca@unal.edu.co>
2021-04-03 13:02:29 -0500

    Mostramos lo que ocurre al declarar y no inicializar una variable. Esto se puede prevenir si desde el comienzo tenemos buenos hábitos para programar.

--- Clase10-03-2021/main.cpp
+++ Clase10-03-2021/main.cpp
@@ -10,6 +10,14 @@
 
 int main(int argc, const char **argv)
 {
-   
+   // Mostraremos como se declara una variable y como se declara e inicializa una variable.
+    int a; // Aqui solo declaramos una variable.
+    int b = 0; // Aqui declaramos e inicializamos una variable, es un buen habito que un programador declare e inicialice una variable ya que es un mal habito dejar una varible la cual pueda tomar cualquier valor. En programas mas complejos y complicados, este mal habito puede traer problemas.
+    std::cout<<"a: "<<a<<"\n";
+    std::cout<<"b: "<<b<<"\n";
     return 0;
 }
+// Al momento de ejecutar este programa el resultado que nos da es el siguiente:
+// a: 0
+// b: 0
+// Lo cual es falso ya que la variable a no posee un valor especifico, este ejemplo, nos empieza a mostrar los posibles fallos que podria tener un programa mas complejo si no utilizamos buenos habitos al programar.

6f8ea2e315d68b96b0a5e726b98a0b26d83ddc6a
Carlosgdc <cdiazca@unal.edu.co>
2021-04-05 14:26:01 -0500

    En este programa aprendimos a como utilizar la función "string", estudiamos para que sirve y algunas aplicaciones de la misma.

--- Clase10-03-2021/main.cpp
+++ Clase10-03-2021/main.cpp
@@ -5,25 +5,28 @@
 //  Created by Countyfoot on 4/3/21.
 //
 
-// Ahora, en este archivo mostraremos lo que ocurre cuando se manejan cantidades mas grandes de lo que puede diferenciar con precisión nuestras funciones. Cuando ocurre esto se conoce como overflow y underflow.
+// Ahora, en este archivo mostraremos lo que ocurre con la función string, en este caso esta función guarda cadenas, donde las cadenas son un arreglo de caracteres especifico, la función string tiene la ventaja que se puede combinar con otras cadenas que hayamos especificado.
 #include<iostream>
-
+#include<string>
 int main(int argc, char **argv)
 {
-    //Mostremos un overflow:
-    int a=2147483647;
-    std::cout<<"a: "<<a<<"\n";
-    a=a+1;                      // El resultado de esta suma es -2147483647
-    std::cout<<"a: "<<a<<"\n";
-    //Mostraremos otro overflow:
-    a=-2147483647;
-    std::cout<<"a: "<<a<<"\n";
-    a=a-1;                      // El resultado de esta resta es 2147483647.
-    std::cout<<"a: "<<a<<"\n";
-    //Mostremos un underflow:
-    float x=1.0e-48;            // El resultado nos da 0.
-    std::cout<<"x: "<<x<<"\n";
-    //Mostraremos otro overflow:
-    x=1.0e+40;                  // El resultado nos da infinito.
-    std::cout<<"x: "<<x<<"\n";
+    std::string name = "Carlos German";
+    std::string lastname = "Diaz";
+    std::string fullname = name + " " + lastname;
+    std::cout<<"fullname: "<<fullname<<"\n";        //El resultado de estas instrucciones es la siguiente: fullname: Carlos German Diaz.
+    std::cout<< fullname[0] <<"\n";   //Lo que ocurrira es que se imprimira solo la primera letra de nuestra cadena, cada caracter ira con un digito asignado, comenzando desde 0. Por lo cual, en este caso el resultado que nos botara sera: C.
+    std::cout<< fullname[3] <<"\n";   //Lo que nos botara en este caso corresponde a la cuarta letra de nuestra cadena, es decir: L.
+    std::cout<<"\n";
+    //Ahora crearemos una función en la cual imprimamos uno por uno los caracteres de nuestra cadena.
+    for(int ii=0;ii<fullname.size(); ii=ii+1)
+    {
+        std::cout<<fullname[ii]<<"\n";
+    }
+    std::cout<<"\n";
+    //Ahora crearemos una función en la cual imprimamos uno por uno los caracteres de nuestra cadena desde atras hacia adelante, es decir, nuestra cadena quedara escrita al reves.
+    for(int ii=fullname.size()-1; ii>=0; ii=ii-1)
+    {
+        std::cout<<fullname[ii]<<"\n";
+    }
+    return 0;
 }
